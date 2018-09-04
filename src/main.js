6e91cc3b244c3dee97ccde318818586abe806ad2
setsunafseisei <380490126@qq.com>
2018-09-04 11:37:07 +0800

    适配 各种大小

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 直接引入 vant 库 和 vant 样式
+/*
+import Vant from 'vant'
+import 'vant/lib/vant-css/index.css'
+Vue.use(Vant)
+*/
+
+// 使用 babel-plugin-import 按需求 引入 vant 中对应的组件（不需要专门引入样式）
+import {Button} from 'vant' // 引入 button
+Vue.use(Button)
+
 
 Vue.config.productionTip = false
 
